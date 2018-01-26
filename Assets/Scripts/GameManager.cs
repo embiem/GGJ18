52f4aa49951e448006630c4306227f9de113b86d
MartinB <mbeierling@gmail.com>
2018-01-26 21:23:41 +0100

    Initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		var i = 1;
+		Debug.Log("It works! " + i);
+		i++;
+		Debug.Log("Now we're done with " + i);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
