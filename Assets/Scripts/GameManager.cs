807e2a243d8ff6e9c8afa80929d8cc53399b9ea8
MartinB <mbeierling@gmail.com>
2018-01-27 18:26:15 +0100

    Add Level1 scene & GameManager improvements

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
+  public static GameManager instance = null;
 
-	public static GameManager instance = null;
 
-	void Start () {
-		instance = this;
-	}
+  [Header("Game Architecture")]
+  public string[] LevelSceneNames = new string[0];
+  public int[] LevelSecondsToPrepare = new int[0];
 
-	public void OnPlayerDied()
+	private int curLevelIndex = 0;
+
+
+  void Start()
+  {
+    if (!instance)
+    {
+      instance = this;
+			DontDestroyOnLoad(this.gameObject);
+
+			// TODO actually load on button press
+			LoadLevel(0);
+    }
+    else
+    {
+      GameObject.Destroy(this);
+    }
+  }
+
+	public void LoadLevel(int levelIndex)
 	{
-		Debug.LogWarning("Player DIED!!");
+		this.curLevelIndex = levelIndex;
+		SceneManager.LoadSceneAsync(LevelSceneNames[levelIndex]);
 	}
+
+  public void OnPlayerDied()
+  {
+    Debug.LogWarning("Player DIED!!");
+  }
 }
