c0534d836d56dff91cf412a5047121269d4fdeaa
MartinB <mbeierling@gmail.com>
2018-01-27 10:50:59 +0100

    Fix turret shooting and add DOTween setup

--- Assets/Scripts/Installments/Turret.cs
+++ Assets/Scripts/Installments/Turret.cs
@@ -2,95 +2,99 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret : MonoBehaviour {
+public class Turret : MonoBehaviour
+{
+  [Header("Refs")]
+  public GameObject projectileSpawnPoint;
 
-    public int health = 3;
+  public int health = 3;
 
-    public float reloadTime = 0.2f;
+  public float reloadTime = 0.2f;
+  public float projectileSpeed = 8f;
 
-    public Rigidbody projectile;
+  public Projectile projectile;
 
-    [Header("Rotation")]
-    public GameObject parent;
+  [Header("Rotation")]
+  public GameObject parent;
 
-    public float maxDegreesPerSecond = 30.0f;
+  public float maxDegreesPerSecond = 30.0f;
 
-    GameObject target;
+  GameObject target;
 
-    GameObject currentTarget;
+  GameObject currentTarget;
 
-    Quaternion qTo;
+  Quaternion qTo;
 
-    bool hasTarget = false;
-
-
-    private void OnTriggerEnter(Collider other)
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.GetComponent<Enemy>())
     {
-     
-        if (other.GetComponent<Enemy>())
-        {
-            if (!hasTarget) {
-                currentTarget = other.gameObject;
-                hasTarget = true;
-                AimAndFire(other.gameObject);
-            }
-           
-        }
+      if (!currentTarget)
+      {
+        currentTarget = other.gameObject;
+        AimAndFire(other.gameObject);
+      }
+
+    }
+  }
+
+  private void OnTriggerExit(Collider other)
+  {
+    if (other.gameObject == currentTarget)
+    {
+      currentTarget = null;
+    }
+  }
+
+  private void Update()
+  {
+    if (currentTarget != null)
+    {
+      Vector3 v3T = currentTarget.transform.position - parent.transform.position;
+      v3T.y = parent.transform.position.y;
+      qTo = Quaternion.LookRotation(v3T, Vector3.up);
+      parent.transform.rotation = Quaternion.RotateTowards(parent.transform.rotation, qTo, maxDegreesPerSecond * Time.deltaTime);
     }
 
-    private void OnTriggerExit(Collider other)
+  }
+
+  void AimAndFire(GameObject enemy)
+  {
+    if (currentTarget)
+      StartCoroutine("ShootAtTarget", enemy.transform.position);
+  }
+
+  IEnumerator ShootAtTarget(Vector3 enemyPos)
+  {
+
+    var spawnedProjectile = Instantiate(
+        projectile,
+        projectileSpawnPoint.transform.position,
+        Quaternion.identity
+    );
+
+    spawnedProjectile.Fire(transform.forward, projectileSpeed);
+
+    yield return new WaitForSeconds(reloadTime);
+
+    if (currentTarget)
     {
-        if (other.gameObject == currentTarget)
-        {
-            hasTarget = false;
-        }
+      AimAndFire(currentTarget);
     }
+  }
 
-    private void Update()
+  public void TakeDamage(int damage)
+  {
+    this.health -= damage;
+    if (this.health <= 0)
     {
-        if (currentTarget != null)
-        {
-            Vector3 v3T = currentTarget.transform.position - parent.transform.position;
-            v3T.y = parent.transform.position.y;
-            qTo = Quaternion.LookRotation(v3T, Vector3.up);
-            parent.transform.rotation = Quaternion.RotateTowards(parent.transform.rotation, qTo, maxDegreesPerSecond * Time.deltaTime);
-        }
-       
+      this.OnDeath();
     }
-
-    void AimAndFire(GameObject enemy)
-    {
-        if (hasTarget)
-        StartCoroutine("ShootAtTarget", enemy.transform.position);
-    }
-
-    IEnumerator ShootAtTarget(Vector3 enemyPos)
-    {
- 
-        Instantiate(projectile, this.transform.position, Quaternion.identity);
-
-        projectile.AddForce(transform.forward * 200 * Time.deltaTime);
-
-        yield return new WaitForSeconds(reloadTime);
-
-        if (hasTarget)
-        {
-            AimAndFire(currentTarget);
-        }
-    }
-
-    public void TakeDamage(int damage)
-    {
-        this.health -= damage;
-        if (this.health <= 0)
-        {
-            this.OnDeath();
-        }
-    }
+  }
 
 
-    private void OnDeath()
-    {
-        Destroy(this.gameObject);
-    }
+  private void OnDeath()
+  {
+    Destroy(this.gameObject);
+  }
 }
